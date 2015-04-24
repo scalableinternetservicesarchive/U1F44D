0758117bb6183161e46b22ad6809e2465bdea744
Braden Anderson <bradencanderson@gmail.com>
2015-04-24 14:22:46 -0700

    React hello world
    I added the react gem to the Gemfile to support React and JSX.
    Then I ran `rails g react:install` to create a `components.js` file that imports all our React components and imports React into `application.js`.
    Then I ran `rails generate react:component App` to generate our top-level App component.

---
+++ app/assets/javascripts/components/app.js.jsx
@@ -0,0 +1,15 @@
+var App = React.createClass({
+
+  render: function() {
+    return (
+      <div>
+        <h1>
+          Main#index
+        </h1>
+        <p>
+          Find me in app/assets/javascripts/components/app.jsx
+        </p>
+      </div>
+    );
+  }
+});
