5755797a47659d4450a6ea7fe6dc59ae2dc53835
Connor Proctor <connorproctor@gmail.com>
2015-04-30 12:03:38 -0700

    Create vote model

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,6 +2,8 @@ class Image < ActiveRecord::Base
   has_attached_file :img
   validates_attachment_content_type :img, :content_type => /\Aimage\/.*\Z/
 
+  has_many :votes
+
   def url
     # @TODO use the right URL...
     return "http://thesnapyak.com/images/" + read_attribute(:id).to_s + ".jpg"
