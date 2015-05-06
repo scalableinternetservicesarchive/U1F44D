ba0500d134eec37d20356e599160838491b74b1b
kaceyjryan <kaceyjryan@gmail.com>
2015-05-06 12:54:10 -0700

    first draft of API stub: API class has functions to getImages, postImage, upvote, and downvote as well as get downvote and upvote status. Still to do: I don't know how the callbacks should work for the POST functions, I haven't handled the error of geolocation being off, and I still need to work on testing all of the functions other than getImages which should work. Currently just a base structure.

--- app/assets/javascripts/api_stub.js
+++ app/assets/javascripts/api_stub.js
@@ -2,43 +2,100 @@
  * Created by kaceyryan on 5/2/15.
  */
 
+var request = require('request');
 
-function get_images(){
+class API {
+  //get images in JSON object for your location
+  getImages(callback) {
     if (navigator.geolocation) {
-        var query = navigator.geolocation.getCurrentPosition(getPosition, showError);
+      navigator.geolocation.getCurrentPosition(function (position) {
+        var query = "/images?lat=".concat(position.coords.latitude, "&long=", position.coords.longitude);
+        //var query = "http://localhost:3000/images?lat=34.0722&long=118.4441";
+        request(query, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            callback(JSON.parse(body));
+          }
+        });
+      }, this.showError);
     } else {
-        //Handle error
-        //x.innerHTML = "Geolocation is not supported by this browser.";
+      //Handle error
+      //x.innerHTML = "Geolocation is not supported by this browser.";
     }
+  }
 
-    var request = require('request');
-    var json_images = request(query, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            return body; // Show the HTML for the Google homepage.
-        }
+  //post an image for your provided location
+  //Does this need a callback?
+  postImage(image, callback){
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        var query = "/images?lat=".concat(position.coords.latitude, "&long=", position.coords.longitude);
+        //var query = "http://localhost:3000/images?lat=34.0722&long=118.4441";
+        request.post({url: query, body: image}, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            //callback(JSON.parse(body));
+            //console.log(body);
+          }
+        });
+      }, this.showError);
+    } else {
+      //Handle error
+      //x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  }
+
+  //get whether you have upvote the post yet in json object
+  getUpvote(id, callback){
+    var query = "/images/".concat(id, "/up");
+    request(query, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        callback(JSON.parse(body));
+      }
     });
+  }
 
-    var json_obj = JSON.parse(json_images);
-    return json_obj;
-}
+  //get whether you have downvoted the post in json
+  getDownvote(id, callback){
+    var query = "/images/".concat(id, "/down");
+    request(query, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        callback(JSON.parse(body));
+      }
+    });
+  }
 
-function getPosition(postion){
-    return str.concat("http://thesnapyak.com/images?lat=", position.coords.latitude, "&long=", postion.coords.longitude);
-}
+  //Does this need a callback?
+  //upvote the image with the given id
+  postUpvote(id){
+    var query = "/images/".concat(id, "/up");
+    request.post(query);
+  }
 
-function showError(error) {
-    switch(error.code) {
-        case error.PERMISSION_DENIED:
-            //x.innerHTML = "User denied the request for Geolocation."
-            break;
-        case error.POSITION_UNAVAILABLE:
-            //x.innerHTML = "Location information is unavailable."
-            break;
-        case error.TIMEOUT:
-            //x.innerHTML = "The request to get user location timed out."
-            break;
-        case error.UNKNOWN_ERROR:
-            //x.innerHTML = "An unknown error occurred."
-            break;
+  //Does this need a callback?
+  //downvote the image with the given id
+  postDownvote(id){
+    var query = "/images/".concat(id, "/down");
+    request.post(query);
+  }
+
+  showError (error) {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+               //x.innerHTML = "User denied the request for Geolocation."
+        break;
+      case error.POSITION_UNAVAILABLE:
+               //x.innerHTML = "Location information is unavailable."
+        break;
+      case error.TIMEOUT:
+               //x.innerHTML = "The request to get user location timed out."
+        break;
+      case error.UNKNOWN_ERROR:
+               //x.innerHTML = "An unknown error occurred."
+        break;
     }
+  }
 }
+
+
+module.exports = new API();
+
+window.API = module.exports;
