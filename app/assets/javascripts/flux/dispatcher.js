46876e1c5111a87c2d854c163db831582d811b91
Braden Anderson <bradencanderson@gmail.com>
2015-05-13 21:43:31 -0700

    better fetch logic

--- app/assets/javascripts/flux/dispatcher.js
+++ app/assets/javascripts/flux/dispatcher.js
@@ -1,3 +1,15 @@
-var Dispatcher = require('flux').Dispatcher;
+var FluxDispatcher = require('flux').Dispatcher;
+
+class Dispatcher extends FluxDispatcher {
+  dispatch(payload) {
+    if (typeof payload === 'string') {
+      super.dispatch({
+        actionType: payload
+      });
+    } else {
+      super.dispatch(payload);
+    }
+  }
+}
 
 module.exports = new Dispatcher();
