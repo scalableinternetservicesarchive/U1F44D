3bd33e792430412adc79d2d8fdad880f424e6be1
Connor Proctor <connorproctor@gmail.com>
2015-05-21 17:01:51 -0700

    delete all the js

--- app/assets/javascripts/main.js
+++
@@ -1,6 +0,0 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-
-// Render top-level React element
-var App = require('./components/app');
-React.render(<App />, document.getElementById('react-node'));
